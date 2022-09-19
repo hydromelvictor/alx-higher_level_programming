7d777ac14294403ca46544531ff003b8ea1b3c90
harkinder-dark <victorvaddely@gmal.com>
2022-09-19 10:49:12 +0000

    begin python programmining

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int check_cycle(listint_t *list)
 {
-    listint_t *current, *head;
+    listint_t *current = malloc(sizeof(listint_t));
+    listint_t *head = malloc(sizeof(listint_t));
     current = list;
     head = current;
     current = current->next;
@@ -11,9 +13,13 @@ int check_cycle(listint_t *list)
     {
         if (current == head)
         {
+            free(current);
+            free(head);
             return (1);
         }
         current = current->next;
     }
+    free(current);
+    free(head);
     return (0);
 }
