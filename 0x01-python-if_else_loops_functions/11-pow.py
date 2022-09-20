66657ecee458d562fe5201e4736b7b68222e61f8
harkinder-dark <victorvaddely@gmal.com>
2022-09-20 12:05:23 +0000

    second day in the python programming

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    res = 1
-    if b == 0:
-        return 1
-    else:
-        if b < 0:
-            a, b = 1/a, - b
-        for i in range(b):
-            res *=a
-    return res
+    return a ** b
