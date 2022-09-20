2fcd5a0cfdbcb6275de9cf98724b98f6b0925d41
harkinder-dark <victorvaddely@gmal.com>
2022-09-20 11:04:40 +0000

    second day in the python programming

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for i in range(10):
     for j in range(10):
         if (i < j):
-            print(f"{i}{j}", end=", ")
+            if i != 8 or j != 9:
+                print("{}{}".format(i, j), end=", ")
+            else:
+                print("{}{}".format(i, j))
