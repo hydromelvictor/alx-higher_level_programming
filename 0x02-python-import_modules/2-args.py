0ab58092313aa3cb08920d28f04e0a2f1dff8fd9
harkinder-dark <victorvaddely@gmal.com>
2022-09-22 09:50:38 +0000

    third day python learning

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
-
-nb_arg = len(sys.argv)
-arg = ""
-if nb_arg == 2:
-    arg = "argument:"
-else:
-    if nb_arg == 1:
-        arg = "arguments."
+if __name__ == "__main__":
+    import sys
+    nb_arg = len(sys.argv)
+    arg = ""
+    if nb_arg == 2:
+        arg = "argument:"
     else:
-        arg = "arguments:"
-print("{} {}".format(nb_arg - 1, arg))
-for i in range(1, nb_arg):
-    print("{}: {}".format(i, sys.argv[i]))
+        if nb_arg == 1:
+            arg = "arguments."
+        else:
+            arg = "arguments:"
+    print("{} {}".format(nb_arg - 1, arg))
+    for i in range(1, nb_arg):
+        print("{}: {}".format(i, sys.argv[i]))
