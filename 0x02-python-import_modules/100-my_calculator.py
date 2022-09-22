ba9af00e6207649fd64d8d68509e73a5b38b9b5f
harkinder-dark <victorvaddely@gmal.com>
2022-09-22 10:11:47 +0000

    third day python learning

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     nb = len(argv)
-    res = "{} + {} = {}"
+    res = "{:d} + {:d} = {:d}"
     if nb == 4:
         match argv[2]:
             case "+":
                 print(res.format(argv[1], argv[3], add(int(argv[1]),
-                                                     int(argv[3]))))
+                                                       int(argv[3]))))
             case "-":
                 print(res.format(argv[1], argv[3], sub(int(argv[1]),
-                                                     int(argv[3]))))
+                                                       int(argv[3]))))
             case "*":
                 print(res.format(argv[1], argv[3], mul(int(argv[1]),
-                                                     int(argv[3]))))
+                                                       int(argv[3]))))
             case "/":
                 print(res.format(argv[1], argv[3], div(int(argv[1]),
-                                                     int(argv[3]))))
+                                                       int(argv[3]))))
             case _:
                 print("Unknown operator. Available operators: +, -,\
                     * and /")
