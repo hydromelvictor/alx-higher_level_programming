a8f9043625b8e129494d4d62e31ddf9f2acecb30
harkinder-dark <victorvaddely@gmal.com>
2022-09-22 10:03:08 +0000

    third day python learning

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
-    arg = sys.argv
-    nb = len(arg)
+    from sys import argv
+    nb = len(argv)
     res = "{} + {} = {}"
     if nb == 4:
-        match arg[2]:
+        match argv[2]:
             case "+":
-                print(res.format(arg[1], arg[3], add(int(arg[1]),
-                                                     int(arg[3]))))
+                print(res.format(argv[1], argv[3], add(int(argv[1]),
+                                                     int(argv[3]))))
             case "-":
-                print(res.format(arg[1], arg[3], sub(int(arg[1]),
-                                                     int(arg[3]))))
+                print(res.format(argv[1], argv[3], sub(int(argv[1]),
+                                                     int(argv[3]))))
             case "*":
-                print(res.format(arg[1], arg[3], mul(int(arg[1]),
-                                                     int(arg[3]))))
+                print(res.format(argv[1], argv[3], mul(int(argv[1]),
+                                                     int(argv[3]))))
             case "/":
-                print(res.format(arg[1], arg[3], div(int(arg[1]),
-                                                     int(arg[3]))))
+                print(res.format(argv[1], argv[3], div(int(argv[1]),
+                                                     int(argv[3]))))
             case _:
                 print("Unknown operator. Available operators: +, -,\
                     * and /")
-                sys.exit(1)
-        sys.exit(0)
+                quit(1)
+        quit(0)
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    quit(1)
