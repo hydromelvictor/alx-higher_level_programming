e59d6fa146f83daa6d327abb862f802d2ffcfafe
harkinder-dark <victorvaddely@gmal.com>
2022-09-25 05:06:28 +0000

    fourth day python learning

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        str = list(my_string)
-        for i in str:
-            if i == 'c' or i == 'C':
-                str.remove(i)
-            my_string = ""
-        for i in str:
-            my_string += i
-        return my_string
+    str = ""
+    for i in my_string:
+        if i != 'c' or i != 'C':
+            str += i
+    return str
