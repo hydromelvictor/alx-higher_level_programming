6dc0d5338fa93affff3e5489f708d4c05e8f1dfa
harkinder-dark <victorvaddely@gmal.com>
2022-09-25 09:50:59 +0000

    fourth day python learning

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,30 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*listint_t *copy(listint_t **head)
+listint_t *reverse_copy(listint_t **head, int n)
+{
+    listint_t *new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+    return (new);
+}
+
+
+listint_t *copy(listint_t **head)
 {
     listint_t *current, *copy = NULL;
     current =*head;
 
     while (current != NULL)
     {
-        add_nodeint_end(&copy, current->n);
+        reverse_copy(&copy, current->n);
         current = current->next;
     }
     return (copy);
-}
-*/
-listint_t *revevrse(listint_t **head)
-{
-    listint_t *start, *current = NULL, *pos;
-    start =*head;
-    while (start != NULL)
-    {
-        pos = start;
-        start = start->next;
-        pos->next = current;
-        current = pos;
-    }
-    return (pos);
 }
 
 int is_palindrome(listint_t **head)
@@ -39,8 +38,7 @@ int is_palindrome(listint_t **head)
         if (start->next == NULL)
             return (0);
 
-        /*current = copy(head);*/
-        current = revevrse(head);
+        current = copy(head);
         while (start->next != NULL)
         {
             if (start->n != current->n)
