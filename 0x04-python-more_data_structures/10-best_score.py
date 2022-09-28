dbadd3c0beb5bfabaa0243de1cbeba8d999d3af0
harkinder-dark <victorvaddely@gmal.com>
2022-09-28 12:57:46 +0000

    fifth day python learning

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    n = 0
     if a_dictionary:
-        return max(a_dictionary)
+        n = max([a_dictionary[key] for key in a_dictionary])
+        for i in a_dictionary:
+            if a_dictionary[i] == n:
+                return i
