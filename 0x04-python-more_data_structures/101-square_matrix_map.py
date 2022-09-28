c011ad965e95eb9ba6b21f43daf03695922c360e
harkinder-dark <victorvaddely@gmal.com>
2022-09-28 15:07:33 +0000

    fifth day python learning

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    str = []
-    for i in matrix[:]:
-        str.append(list(map(lambda j: j**2, i)))
-    return str
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
