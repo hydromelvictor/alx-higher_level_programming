0648a5391826ee2b3d3358c30be22ee7a9f2ba5d
harkinder-dark <victorvaddely@gmal.com>
2022-09-28 13:55:33 +0000

    fifth day python learning

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
-    if type(roman_string) == str:
+    if type(roman_string) != str:
         return 0
     ch = []
     for i in list(roman_string):
@@ -10,3 +10,18 @@ def roman_to_int(roman_string):
         if ch[i] < ch[i + 1]:
             ch[i] = -ch[i]
     return sum(ch)
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
