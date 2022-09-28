a8e2128eb07f9e7a22ba76061a429da4aed1257b
harkinder-dark <victorvaddely@gmal.com>
2022-09-28 13:57:20 +0000

    fifth day python learning

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,18 +10,3 @@ def roman_to_int(roman_string):
         if ch[i] < ch[i + 1]:
             ch[i] = -ch[i]
     return sum(ch)
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
