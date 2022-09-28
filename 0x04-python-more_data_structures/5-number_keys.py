d612621551f75e5e504fd37799d8ba9ebe17be70
harkinder-dark <victorvaddely@gmal.com>
2022-09-28 11:43:29 +0000

    fifth day python learning

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     return len(a_dictionary)
-
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
