a94bce6607596cdc37475252976154513aac33f7
harkinder-dark <victorvaddely@gmail.com>
2022-10-10 19:31:27 +0000

    5th day in python programming

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
         except IndexError:
             print("")
             return i
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,21 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     for j in range(x):
-        if type(my_list[j]) == int:
+        if
             print("{:d}".format(my_list[j]), end="")
             i += 1
     print("")
     return i
-        
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
