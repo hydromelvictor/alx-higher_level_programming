5ffe1309a9b91f182e89dabc02523f82cf768d35
harkinder-dark <victorvaddely@gmail.com>
2022-10-10 13:45:42 +0000

    5th day in python programming

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x <= 0:
-        return 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
