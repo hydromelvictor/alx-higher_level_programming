8b17ddd85c16ca32e5ae6d599d7326fca97fd84c
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 08:15:33 +0000

    5th day in python programming

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+import dis
+
 def magic_calculation(a, b):
+    result = 0
+    for i in range(1,3):
+        a = i
+
+
+dis.dis(magic_calculation)
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+6th days python programming
