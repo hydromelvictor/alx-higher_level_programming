103c876add8f7fb75188b40ec72306135a6b73c9
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 21:58:15 +0000

    6th day in python proggramming

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """tache 1 description d'une class vide"""
-
 class Square:
     """class decrivant un carre"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""tache 2 description d'une class vide"""
 class Square:
-    def __init__(self):
-        pass
+    """ definition de la fonction init de square"""
+    def __init__(self, size):
+        self.__size = size
