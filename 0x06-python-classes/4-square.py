7bd39713b7527a9a6a951b2c723712921f157659
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 22:41:52 +0000

    6th day in python proggramming

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,17 +30,3 @@ class Square:
         else:
             self.__size = value
         return self.__size
-
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
