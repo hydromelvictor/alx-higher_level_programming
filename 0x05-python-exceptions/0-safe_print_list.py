40275b441a0ce0af106c02ddad20628b6444beb2
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 01:14:53 +0000

    5th day in python programming

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x < 0:
-        return 0
+    j = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            j += 1
         except IndexError:
-            print("")
-            return i
+            break
     print("")
-    return i + 1
+    return j
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    for j in range(x):
-        if
-            print("{:d}".format(my_list[j]), end="")
-            i += 1
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[x]), end="")
+            j += 1
+        except TypeError:
+            continue
     print("")
-    return i
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    return j
