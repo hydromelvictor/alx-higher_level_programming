7530618b4df1ec7523db3ba59f7ae7f85d435613
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 07:14:57 +0000

    5th day in python programming

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    a = None
+    try:
+        a = fct(*args)
+    except BaseException as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        a = None
+    return a
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[x]), end="")
+            print("{:d}".format(my_list[i]), end="")
             j += 1
-        except TypeError:
+        except (TypeError, ValueError):
             continue
-    print("")
+    print()
     return j
