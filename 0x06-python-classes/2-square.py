788f9078aa10c9fbfd406c1e6d735250c0ae8f91
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 22:12:12 +0000

    6th day in python proggramming

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""tache 2 description d'une class vide"""
+class Square:
+    """ definition de la fonction init de square"""
+    def __init__(self, size=0):
+        """init function"""
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+        
