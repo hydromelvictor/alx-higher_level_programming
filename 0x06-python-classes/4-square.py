1cc73ed67fed6c67cdc97a3402788c56b64e9d4a
harkinder-dark <victorvaddely@gmail.com>
2022-10-11 23:40:59 +0000

    6th day in python proggramming

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     def area(self):
         """the square area"""
         return self.__size ** 2
