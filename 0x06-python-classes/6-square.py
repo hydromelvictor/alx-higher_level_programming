83cde30ea35d38ea647c1eac7530b5ed89cb6377
harkinder-dark <victorvaddely@gmail.com>
2022-10-12 09:27:03 +0000

    6th day in python proggramming

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,8 +44,10 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for l in range(self.__position[1]):
-            print()
+        
+        if self.__position[1] != 0:
+            for i in range(self.__position[1]):
+                print()
 
         for i in range(self.__size):
             for k in range(self.__position[0]):
