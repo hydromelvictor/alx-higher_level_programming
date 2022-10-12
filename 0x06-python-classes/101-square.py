95d29e645c815e578065e662a69ab449ba20f311
harkinder-dark <victorvaddely@gmail.com>
2022-10-12 09:02:16 +0000

    6th day in python proggramming

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""tache 2 description d'une class vide"""
+
+
+class Square:
+    """ definition de la fonction init de square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """init function"""
+
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif not (isinstance(position, tuple)) or len(position) != 2 \
+                or position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__size = size
+            self.__position = position
+
+    def area(self):
+        """the square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter function for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter function for size"""
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+        return self.__size
+
+    def my_print(self):
+        """printf function"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
+
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        """getters functiion for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter functions for position"""
+        if not (isinstance(value, tuple)) or len(value) != 2 or value[0] < 0 \
+                or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+        return self.__position
+
+    def __str__(self):
+        string = ""
+        if self.__size == 0:
+            return "\n"
+        else:
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    string += " "
+                for k in range(self.__size):
+                    string += "#"
+                string += "\n"
+        return string
