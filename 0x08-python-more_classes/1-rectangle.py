f442188973225bc12cce07c72c0b42e758b960f9
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-17 20:01:11 +0000

    8th day in python programming

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,13 +4,14 @@
     rectangle classs
 """
 
+
 class Rectangle:
     """
         rectangle class define
     """
     def __init__(self, width=0, height=0):
         """
-            initialisation 
+            initialisation
         """
         if type(width) is not int:
             raise TypeError("width must be an integer")
@@ -34,7 +35,6 @@ class Rectangle:
         """
         return self.__width
 
-
     @width.setter
     def width(self, value):
         """
@@ -48,14 +48,12 @@ class Rectangle:
 
         self.__width = value
 
-
     @property
     def height(self):
         """
             height getter
         """
         return self.__height
-
 
     @height.setter
     def height(self, value):
