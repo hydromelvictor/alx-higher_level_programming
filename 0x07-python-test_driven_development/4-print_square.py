fa3a04f04de1a83bff6e14ec6b7196d550d5759d
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-17 10:03:30 +0000

    7th day in python programming

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
 """
-
+    square function programming
 """
 
 
 def print_square(size):
     """
-    
+        size: square size
+        print the square show in '#'
     """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise TypeError("size must be >= 0")
+
+    for i in range(size):
+        print("{}".format("#" * size))
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python
+
+"""
+    function print the arguments text with 
+    back new ligne if character is '.' or ':' or '?' 
+"""
+
+
+def text_indentation(text):
+    """
+        text: text aguments
+        print text
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    s = ""
+
+    for i in text:
+        if i == "." or i == "?" or i == ":":
+            s += i
+            print("{}".format(s.strip()))
+            print(end="\n")
+            s = ""
+        else:
+            s += i
+    print("{}".format(s.strip()), end="")
