ad7b9db12d543138507cb772f2d6c0d13b73b2c5
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-17 16:04:14 +0000

    7th day in python programming

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,1 +1,62 @@
 #!/usr/bin/python
+
+"""
+
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    
+    """
+    if type(m_a) is not list or type(m_b) is not list:
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    for i in m_a:
+        if type(i) is not list:
+            raise TypeError("m_a must be a list of lists")
+
+        if i is None:
+            raise ValueError("m_a can't be empty")
+
+        size = len(m_a[0])
+        
+        for j in i:
+            if j is None:
+                raise ValueError("m_a can't be empty")
+
+            if type(j) is not int and type(j) is not float:
+                raise TypeError("m_a should contain only integers or floats")
+
+        if len(i) != size:
+            raise TypeError("each row of m_a must be of the same size")
+
+
+    for j in m_b:
+        if type(j) is not list:
+            raise TypeError("m_b must be a list of lists")
+
+        if j is None:
+            raise ValueError("m_b can't be empty")
+
+        size = len(m_b[0])
+        
+        for k in j:
+            if k is None:
+                raise ValueError("m_b can't be empty")
+
+            if type(k) is not int and type(k) is not float:
+                raise TypeError("m_b should contain only integers or floats")
+
+        if len(j) != size:
+            raise TypeError("each row of m_b must be of the same size")
+
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    m = []
+
+
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+"""
+    unittest for function max_integer
+"""
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+        test class for max_integer
+        subclass of de unittest
+    """
+
+    def doc_module(self):
+        """
+            docstring of module verificaton
+        """
+        m = __import__("6-max_integer").__doc__
+        self.assertTrue(len(m) > 1)
+
+
+    def doc_function(self):
+        """
+            docstring of function verification
+        """
+        m = __import__("6-max_integer").max_integer.__doc__
+        self.assertTrue(len(m) > 1)
+
+
+    def example1(self):
+        """
+            empty list
+        """
+        n = []
+        self.assertIsNone(max_integer(n))
+
+
+    def example2(self):
+        """
+            list of [1, 2, 3]
+        """
+        n = [1, 2, 3]
+        self.assertEqual(max_integer(n), 3)
+
+
+    def example3(self):
+        """
+            list of [7, -12, 4.7, 0]
+        """
+        n = [7, -12, 4.7, 0]
+        self.assertEqual(max_integer(n), 7)
+
+
+    def example4(self):
+        """
+            list of [2.8]
+        """
+        n = [2.8]
+        self.assertEqual(max_integer(n), 2.8)
+
+
+    def example5(self):
+        """
+            list of [7, "miami", [4, -85], 0]
+        """
+        n = [7, "miami", [4, -85], 0]
+        with self.assertRaises(TypeError):
+            max_integer(n)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
