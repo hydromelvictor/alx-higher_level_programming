e9c1630f29e29f3f8826be46c0209871bf2d2450
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-18 13:04:24 +0000

    8th day in python programming

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+s = []
+def magic_string():
+    s.append("BestSchool")
+    return ", ".join(s)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    """
+        lockedclass
+        __init__ function
+    """
+    __slots__ = ('first_name', )
