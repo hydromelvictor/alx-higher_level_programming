03f3b5b83d8e64bf448d5c195e6578ba2c8ee721
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-19 04:37:20 +0000

    8th day in python programming

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+    Doc
+"""
+
 
 def max_integer(list=[]):
     """
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
     unittest for function max_integer
 """
 
+
 import unittest
 max_integer = __import__("6-max_integer").max_integer
 
