7fe7c5f4ebdde88fb979a5bfae289827acbe93a4
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-19 04:09:10 +0000

    8th day in python programming

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,11 +5,12 @@
     a, b is integer or float number otherwise raise
 """
 
+
 def add_integer(a, b=98):
     """return a + b if the conditions is ok"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    
+
     if type(a) is float:
         a = int(a)
 
