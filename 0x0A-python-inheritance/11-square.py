72f24b67cda87b1d9727ef6d40f4d17ff65fe667
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-24 18:04:45 +0000

    nth day in C programming

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """size: limit
         """
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
 
     def area(self):
         """area for square
