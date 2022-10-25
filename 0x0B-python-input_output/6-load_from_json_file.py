abe30258ea9972a9d79b1e7480749cf201fd9865
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 17:13:54 +0000

    nth day in python programming

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,5 @@ import json
 def load_from_json_file(filename):
     """filename: name of file
     """
-    return json.load(filename)
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
