7d92c895b44db5bdcac2c1c5c461cc6b9f0a7685
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 16:08:51 +0000

    nth day in python programming

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""
+"""writen in the file
 """
 
 
 def write_file(filename="", text=""):
+    """ filename: name of file
+        text: word from file
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
