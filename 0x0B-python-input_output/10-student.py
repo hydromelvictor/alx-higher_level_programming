acf95ad1bce49c1bebeb5ad0e289c9755588df58
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 18:45:59 +0000

    nth day in python programming

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" student class
+"""
+
+
+class Student:
+    """student class defined
+    """
+    def __init__(self, first_name, last_name, age):
+        """ first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json
+        """
+        if type(attrs) is list and type(attrs[0]) is str:
+            return attrs.__dict__
+
+        return self.__dict__
