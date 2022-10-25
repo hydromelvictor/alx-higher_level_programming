4102e812127d040fa1baeeb5eaceddecb07d7a58
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 17:36:13 +0000

    nth day in python programming

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,14 @@
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys
 
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
 
-def add_item():
-    """
-    """
+for arg in sys.argv:
+    my_list.append(arg)
+
+save_to_json_file(my_list, "add_item.json")
