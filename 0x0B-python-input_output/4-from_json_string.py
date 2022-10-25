a040eaf2bc7faa8c5be15f1929afce0b883927f4
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 16:48:52 +0000

    nth day in python programming

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,9 +5,8 @@
 from io import StringIO
 import json
 
+
 def from_json_string(my_str):
     """ my_str: data format serialize in json
     """
-    io = StringIO()
-    json.dump(my_str, io)
-    return io.getvalue()
+    return json.loads(my_str)
