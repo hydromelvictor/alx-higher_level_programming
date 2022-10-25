a4d33b329be526e43afb294fd317aa51249eae95
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-25 20:26:12 +0000

    nth day in python programming

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""append after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ filename: name of file
+        search_string: string
+        new_string: string  new
+    """
+    with open(filename, "r+", encoding="utf-8") as file:
+        for line in file:
+            if line == search_string:
+                line += "\n" + new_string + "\n"
