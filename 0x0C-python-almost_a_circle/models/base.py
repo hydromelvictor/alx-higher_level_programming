bc172885d7ee318f7050c29486f28a78e64d89e4
hydromel-andromark <dolk.svictor@gmail.com>
2022-10-29 08:02:18 +0000

    nth day in python programming

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """class base
 """
 
+import json
+
 
 class Base:
     """base class
@@ -16,3 +18,41 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert to json
+        """
+        if list_dictionaries is None:
+            return json.dumps([])
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file json
+        """
+        filename = cls.__name__ + ".json"
+        
+        if list_objs is None:
+            list_objs = []
+
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """deserialization of json_string 
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creating functions
+        """
+        pass
+
+
