8741e88067febac4b0e050bb79684060454ae645
harkinder-dark <victorvaddely@gmail.com>
2022-10-31 19:28:31 +0000

    nth programming in python learning

--- 0x0C-python-almost_a_circle/models/rectanlge.py
+++ 0x0C-python-almost_a_circle/models/rectanlge.py
@@ -6,4 +6,11 @@ from base import Base
 
 class Rectanlge(Base):
     """class heritant de base"""
-    pass
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initiaidsation"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
