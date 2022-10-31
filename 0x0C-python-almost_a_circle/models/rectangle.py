22b5008ba0e6808137bf81d5174be1c2c7abd4ec
harkinder-dark <victorvaddely@gmail.com>
2022-10-31 19:39:52 +0000

    nth programming in python learning

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,85 @@
 """rectangle class
 """
 
-from base import Base
+import json
+import os
+
+
+class Base:
+    """base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """id: id for init
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert to json
+        """
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file json
+        """
+        filename = cls.__name__ + ".json"
+        obj = cls.to_json_string(list_objs)
+
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(obj)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """deserialization of json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creating functions
+        """
+        str = f"[{cls.__name__}]"
+        if "id" in dictionary:
+            str += f" ({dictionary['id']})"
+
+        if "x" in dictionary:
+            str += f" {dictionary['x']}/"
+
+        if "y" in dictionary:
+            str += f"{dictionary['y']} -"
+
+        if "width" in dictionary:
+            str += f" {dictionary['width']}/"
+
+        if "height" in dictionary:
+            str += f"{dictionary['height']}"
+
+        return str
+
+    @classmethod
+    def load_from_file(cls):
+        """loads form file"""
+        filename = cls.__name__ + ".json"
+
+        if os.path.exists(filename):
+            with open(filename, "r", encoding="utf-8") as file:
+                for i in file:
+                    cls.from_json_string(i)
+        else:
+            return []
 
 
 class Rectangle(Base):
