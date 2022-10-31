0e34377d7925d058f330e068ddd8c9adc46e135f
harkinder-dark <victorvaddely@gmail.com>
2022-10-31 19:59:15 +0000

    nth day programming in python

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -217,9 +217,8 @@ class Rectangle(Base):
     def __str__(self):
         """string format methode
         """
-        return "\
-        [Rectangle] ({}) {}/{} - {}/{}\
-        ".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """ updating
