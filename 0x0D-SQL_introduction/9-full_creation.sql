ecd83f91a2c5453769ba90806b81d8fd4e8e8fa9
harkinder-dark <victorvaddely@gmail.com>
2022-11-29 17:52:01 +0000

    program

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,5 @@
 -- creates a table  in the database
 CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
--- add into the tables
 INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (1, “John”, 10);
 INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (2, “Alex”, 3);
 INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (3, “Bob”, 14);
