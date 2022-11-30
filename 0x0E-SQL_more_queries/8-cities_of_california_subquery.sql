a88fdb5cbef2fe02be66e24edc9c3892039b5bbe
harkinder-dark <victorvaddely@gmail.com>
2022-11-30 21:53:41 +0000

    sql second day

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,9 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT `cities`.`id`, `cities`.`name`
 FROM `cities`
-WHERE `cities`.`state_id` = `states`.`id` AND `states`.`name` = "California"
-ORDER BY `cities`.`id` ASC;
+WHERE `cities`.`state_id` IN (
+    SELECT `id`
+    FROM `states`
+    WHERE `name` = "California"
+    )
+ORDER BY `cities`.`id`;
