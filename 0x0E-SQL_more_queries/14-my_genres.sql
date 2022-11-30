cb2a6f45d56a3f7bab13130e8ae5a3178da11b87
harkinder-dark <victorvaddely@gmail.com>
2022-11-30 21:38:31 +0000

    sql second day

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,9 +2,9 @@
 SELECT tvg.`name`
 FROM `tv_genres` AS tvg 
     INNER JOIN `tv_show_genre` AS tvs
-    ON tvs.`id` = tvs.`genre_id`
+    ON tvg.`id` = tvs.`genre_id`
 
     INNER JOIN `tv_shows` AS tv
     ON tv.`id` = tvs.`show_id`
-    WHERE tvs.`title` = "Dexter"
+    WHERE tv.`title` = "Dexter"
 ORDER BY tvg.`name` ASC;
