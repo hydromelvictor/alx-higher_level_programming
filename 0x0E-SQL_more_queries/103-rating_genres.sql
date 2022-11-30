232041fd940482c396f08b697903ca9d6d51ee11
harkinder-dark <victorvaddely@gmail.com>
2022-11-30 21:34:13 +0000

    sql second day

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate
-SELECT `name`, SUM(`rate`) AS `rating`
-    FROM `tv_genres`
+SELECT tvg.`name`, SUM(tvr.`rate`) AS `rating`
+    FROM `tv_genres`AS tvg
         INNER JOIN `tv_show_genres` AS tvs
-        ON tvs.`genre_id` = `id`
+        ON tvs.`genre_id` = tvg.`id`
 
         INNER JOIN `tv_show_rating` AS tvr
         ON tvs.`show_id` = tvr.`show_id`
