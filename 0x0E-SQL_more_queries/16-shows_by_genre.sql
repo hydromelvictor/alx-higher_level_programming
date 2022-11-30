0f882927fe67574c36bcd47aecff9aaed562d11b
harkinder-dark <victorvaddely@gmail.com>
2022-11-30 21:21:34 +0000

    sql second day

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows, and all genres linked to that show
-SELECT tv.`title`, tvg.`name`
+SELECT `tv_shows`.`title`, `tv_show_genres`.`name`
 FROM `tv_shows` AS tv
     LEFT JOIN `tv_show_genres` AS tvs
     ON tv.`id` = tvs.`show_id`
