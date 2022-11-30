f305183b81392c89fb4dae3ab39901a565eab843
harkinder-dark <victorvaddely@gmail.com>
2022-11-30 21:57:57 +0000

    sql second day

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,10 @@ CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
     IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-        PRIMARY KEY(`id`)
-        `id`        INT  UNIQUE     AUTO_INCREMENT          NOT NULL,
-        `state_id`  INT  NOT NULL   FOREIGN KEY(`state_id`)
-            REFERENCES `hbtn_0d_usa`.`states`(`state_id`),
-        `name`      VARCHAR(256)    NOT NULL 
+        PRIMARY KEY(`id`),
+        `id`        INT   AUTO_INCREMENT   NOT NULL,
+        `state_id`  INT  NOT NULL,
+        `name`      VARCHAR(256)    NOT NULL,
+        FOREIGN KEY(`state_id`)
+        REFERENCES `hbtn_0d_usa`.`states`(`state_id`)
     );
