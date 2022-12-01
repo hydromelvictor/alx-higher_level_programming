0b5d2421a097eebffd44991037808014da5cb4ba
harkinder-dark <victorvaddely@gmail.com>
2022-12-01 07:08:58 +0000

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- creates the MySQL server user
-CREATE USER 
+CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost' 
-    IDENTIFIED BY 'user_0d_1_pwd';
+        IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES
 ON *.*
-TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
