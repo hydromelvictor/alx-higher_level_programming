0ac0f4d20825835f28d3706a97a0ceb1c4b7f38d
harkinder-dark <victorvaddely@gmail.com>
2022-12-01 08:13:35 +0000

    sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,7 @@ FROM `tv_genres`
 
     INNER JOIN `tv_shows`
     ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
-    WHERE `tv_shows`.`name` NOT IN (
+    WHERE `tv_shows`.`title` NOT IN (
         SELECT `tv_genres`.`name`
         FROM `tv_genres`
             INNER JOIN `tv_show_genres`
