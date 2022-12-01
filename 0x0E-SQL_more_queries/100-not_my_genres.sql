944ec0dbdb99422661da96c1cc8bc99b3cc55244
harkinder-dark <victorvaddely@gmail.com>
2022-12-01 07:55:22 +0000

    sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,19 +1,19 @@
 -- uses the hbtn_0d_tvshows database to list all genres
-SELECT DISTINCT tvg.`name`
-FROM `tv_genres` AS tvg
-    INNER JOIN `tv_show_genres` AS tvs
-    ON tvg.`id` = tvs.`genre_id`
+SELECT DISTINCT `tv_genres`.`name`
+FROM `tv_genres`
+    INNER JOIN `tv_show_genres`
+    ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 
-    INNER JOIN `tv_shows` AS tv
-    ON tv.`id` = tvs.`show_id`
-    WHERE tv.`name` NOT IN (
-        SELECT tvg.`name`
-        FROM `tv_genres` AS tvg
-            INNER JOIN `tv_show_genres` AS tvs
-            ON tvg.`id` = tvs.`genre_id`
+    INNER JOIN `tv_shows`
+    ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+    WHERE `tv_shows`.`name` NOT IN (
+        SELECT `tv_genres`.`name`
+        FROM `tv_genres`
+            INNER JOIN `tv_show_genres`
+            ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 
-            INNER JOIN `tv_shows` AS tv
-            ON tv.`id` = tvs.`show_id`
-            WHERE tv.`name` = "Dexter"
+            INNER JOIN `tv_shows`
+            ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+            WHERE `tv_shows`.`name` = "Dexter"
     )
-ORDER BY tvg.`name` ASC;
+ORDER BY `tv_genres`.`name` ASC;
