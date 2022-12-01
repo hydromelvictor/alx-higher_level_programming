60e274cbbcb299b0c6c30ddf99b08cf84b4f3724
harkinder-dark <victorvaddely@gmail.com>
2022-12-01 07:12:50 +0000

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,7 +4,7 @@ CREATE USER
     IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
+    ON *.*
+    TO 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
