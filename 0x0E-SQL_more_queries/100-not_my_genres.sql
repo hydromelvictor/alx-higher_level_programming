a4fcdbbc0496037ce66385e32941652ab264e427
harkinder-dark <victorvaddely@gmail.com>
2022-12-01 08:14:33 +0000

    sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -14,6 +14,6 @@ FROM `tv_genres`
 
             INNER JOIN `tv_shows`
             ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
-            WHERE `tv_shows`.`name` = "Dexter"
+            WHERE `tv_shows`.`title` = "Dexter"
     )
 ORDER BY `tv_genres`.`name` ASC;
