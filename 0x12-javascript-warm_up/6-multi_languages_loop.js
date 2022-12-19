d78997eba530c536a4e1bd220b2607312a042beb
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:12:47 +0000

    task 0-13

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let n = 0;
-while (n < myVar.length)
-{
+while (n < myVar.length) {
   console.log(myVar[n]);
   n++;
 }
