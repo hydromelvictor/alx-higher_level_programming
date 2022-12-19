df8e25e919b4412e35bfe7d146cde8c54f10d32a
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:40:02 +0000

    task 0-13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,35 +1,28 @@
 #!/usr/bin/node
-function pusher(args)
-{
-    let nbre = [];
-    let n = args.length;
-    let i = 2;
-    while (i < n)
-    {
-        nbre.push(Math.floor(Number(args[i])));
-	    i++;
-    }
-    return nbre;
+function pusher (args) {
+  let nbre = [];
+  let n = args.length;
+  let i = 2;
+  while (i < n) {
+    nbre.push(Math.floor(Number(args[i])));
+    i++;
+  }
+  return nbre;
 }
 
-function maxi(nbre)
-{
-    let max = 2;
-    let i;
-    for(i = 2; i < nbre.length; i++)
-    {
-        if (nbre[max] < nbre[i]) max = i; 
-    }
-    return max;
+function maxi (nbre) {
+  let max = 2;
+  for (let i = 2; i < nbre.length; i++) {
+    if (nbre[max] < nbre[i]) max = i; 
+  }
+  return max;
 }
 
 let i = 0;
-if (process.argv.length <= 3)
-{
-    console.log(0);
-}else{
-    let n = pusher(process.argv);
-    n.pop(maxi(n));
-    console.log(process.argv[maxi(n)]);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  let n = pusher(process.argv);
+  n.pop(maxi(n));
+  console.log(process.argv[maxi(n)]);
 }
-
