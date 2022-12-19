03e2e52477c327ef658eebe8455f63d3273d4938
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:18:26 +0000

    task 0-13

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 let n = Math.floor(Number(process.argv[2]));
-if (n)
-{
-    while (n < 0)
-    {
-        console.log('C is fun')
-    }
-}else{
-    console.log('Missing number of occurrences');
+if (n) {
+  while (n < 0) {
+    console.log('C is fun')
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
