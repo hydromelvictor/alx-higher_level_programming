c7cdfba83d1721dbf5ff0f6e825c2a8200dd6a8a
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:28:56 +0000

    task 0-13

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-function add(a, b)
-{
-    return (a + b);
+function add (a, b) {
+  return (a + b);
 }
-
-let a = Math.floor(Number(process.argv[2]));
-let b = Math.floor(Number(process.argv[3]));
-
-if (a && b)
-{
-    console.log(add(a, b));
-}else{
-    console.log(Math.floor(NaN));
+const a = Math.floor(Number(process.argv[2]));
+const b = Math.floor(Number(process.argv[3]));
+if (a && b) {
+  console.log(add(a, b));
+} else {
+  console.log(Math.floor(NaN));
 }
