4bac53c6cd07a81cd545dbf90b0de36911e75f95
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 18:59:12 +0000

    task 0-13

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 let myvar;
-if (process.argv.length === 2){ 
-	console.log(`${myvar} is ${myvar}`);
-}else{
-	console.log((!process.argv[3]) ? `${process.argv[2]} is ${myvar}`: `${process.argv[2]} is ${process.argv[3]}`);
+if (process.argv.length === 2) { 
+  console.log(`${myvar} is ${myvar}`);
+} else {
+  console.log((!process.argv[3]) ? `${process.argv[2]} is ${myvar}`: `${process.argv[2]} is ${process.argv[3]}`);
 }
 
