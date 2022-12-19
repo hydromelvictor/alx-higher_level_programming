4181afe9e24ff3c3c209f71d7ec3c2d5564ac8d8
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 20:01:16 +0000

    task 0-13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,28 +1,9 @@
 #!/usr/bin/node
-function pusher (args) {
-  let nbre = [];
-  let n = args.length;
-  let i = 2;
-  while (i < n) {
-    nbre.push(Math.floor(Number(args[i])));
-    i++;
-  }
-  return nbre;
-}
-
-function maxi (nbre) {
-  let max = 2;
-  for (let i = 2; i < nbre.length; i++) {
-    if (nbre[max] < nbre[i]) max = i; 
-  }
-  return max;
-}
-
-let i = 0;
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let n = pusher(process.argv);
-  n.pop(maxi(n));
-  console.log(process.argv[maxi(n)]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
