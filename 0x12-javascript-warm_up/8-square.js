7873096db3a744de659ee74013f4e67bbcd67d20
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:25:57 +0000

    task 0-13

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 let n = Math.floor(Number(process.argv[2]));
 let m;
-let f = n;
-if (n)
-{
-    while (n > 0)
-    {
-	    m = '';
-	    for (let i = 0; i < f; i++) m += 'X';
-	    console.log(m);
-	n--;        
-    }
-}else{
-    console.log('Missing size');
+const f = n;
+if (n) {
+  while (n > 0) {
+    m = '';
+    for (let i = 0; i < f; i++) m += 'X';
+    console.log(m);
+    n--;
+  }
+} else {
+  console.log('Missing size');
 }
