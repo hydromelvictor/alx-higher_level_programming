2b2207f385003de3e809cab5a466f695f9afea75
harkinder-dark <victorvaddely@gmail.com>
2022-12-19 19:31:28 +0000

    task 0-13

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-function factoriel(a)
-{
-    if (isNaN(a) || a === 0) return 1;
-    return (a * factoriel(a -1));
+function factoriel (a) {
+  if (isNaN(a) || a === 0) return 1;
+  return (a * factoriel(a - 1));
 }
-
-let a = Math.floor(Number(process.argv[2]));
+const a = Math.floor(Number(process.argv[2]));
 console.log(factoriel(a));
