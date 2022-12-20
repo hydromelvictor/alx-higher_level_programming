3739d3a8335cb1050b164f6ab14cfcfc0ada1924
harkinder-dark <victorvaddely@gmail.com>
2022-12-20 16:43:38 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor (h, w) {
-        this.width = w;
-        this.height = h;
-    }
-}
+module.exports = class Rectangle {
+  constructor (h, w) {
+    this.width = w;
+    this.height = h;
+  }
+};
