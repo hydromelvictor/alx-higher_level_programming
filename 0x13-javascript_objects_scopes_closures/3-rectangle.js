6955b9da0283df0ce53a044d2d41f94324935e1b
harkinder-dark <victorvaddely@gmail.com>
2022-12-20 17:03:31 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (typeof(h) === 'number' && typeof(w) === 'number' && h > 0 && w > 0) {
+    if (typeof (h) === 'number' && typeof (w) === 'number' && h > 0 && w > 0) {
       this.width = w;
       this.height = h;
     }
