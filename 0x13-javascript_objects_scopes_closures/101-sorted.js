6c4a7394b12015cff935d25da32d22391491eccc
harkinder-dark <victorvaddely@gmail.com>
2022-12-20 17:51:50 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const dict = require('./101-data');
+const dict = require('./101-data').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === 'undefined') {
+  if (newDict[dict[key]] === undefined) {
 	  new:Dict[dict[key]] = [key];
   } else {
     newDict[dict[key]].push(key);
