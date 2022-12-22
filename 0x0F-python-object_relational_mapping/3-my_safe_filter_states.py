f0b75a2fe14bd8696572013f916212f57de2047c
harkinder-dark <victorvaddely@gmail.com>
2022-12-22 09:22:55 +0000

    python programming relationnal mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""But this time, write one that is safe from MySQL injections"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur  = conn.cursor()
+    cur.execute("""SELECT * FROM `states` 
+                WHERE `name` == sys.argv[4] 
+                ORDER BY id ASC
+                """)
+    query_rows = cur.fetchall()
+    [print(row) for row in query_rows]
