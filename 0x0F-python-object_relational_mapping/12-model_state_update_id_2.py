8e080c94c4d36daea404bba56c9092d244fdb1f1
harkinder-dark <victorvaddely@gmail.com>
2022-12-22 13:22:45 +0000

    coorect

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,8 +10,8 @@ from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ = '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,8 +11,8 @@ from model_city import City
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ = '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
 
