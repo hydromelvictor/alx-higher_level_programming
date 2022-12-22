6ac130c5006185d75b9ddebafd91ec37a4f01a9f
harkinder-dark <victorvaddely@gmail.com>
2022-12-22 14:36:54 +0000

    correct

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State):
