79dbd47acba72b9b4e39e5c20c732d3ed318e862
harkinder-dark <victorvaddely@gmail.com>
2022-12-22 10:54:55 +0000

    change mode

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM `states`\
-            WHERE `name`='{sys.argv[4]}' ORDER BY id")
+    cur.execute("SELECT * FROM `states`\
+                 WHERE `name`='{}'".format(sys.argv[4]))
     query_rows = cur.fetchall()
     [print(row) for row in query_rows]
