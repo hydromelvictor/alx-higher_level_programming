51a6cd14edd4b88043d0e190671759a7ed12d47a
harkinder-dark <victorvaddely@gmail.com>
2022-12-22 11:15:00 +0000

    correction

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM `states` WHERE `name` == sys.argv[4]\
-                 ORDER BY id")
+    cur.execute("SELECT * FROM `states` ORDER BY id")
     query_rows = cur.fetchall()
-    [print(row) for row in query_rows]
+    [print(row) for row in query_rows if row[1] == sys.argv[4]]
