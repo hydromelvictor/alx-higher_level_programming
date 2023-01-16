f180cdb79716496d1d5f07814ee137f234cd9b16
harkinder-dark <victorvaddely@gmail.com>
2023-01-16 13:09:57 +0000

    python netw

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,20 +6,21 @@ def find_peak(list_of_integers):
     """list_of_integers: list -> int
     return: int
     """
-    if len(list_of_integers) == 0:
+    size = len(list_of_integers)
+    if size == 0:
         return None
-    if len(list_of_integers) == 1:
+    if size == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    if size == 2:
         return max(len_of_integers)
 
-    n = int(len(list_of_integers) / 2)
+    n = int(size / 2)
     peak = list_of_integers[n]
     
     if peak > list_of_integers[n - 1] and peak > list_of_integers[n + 1]:
         return peak
     elif peak < list_of_integers[n - 1]:
-        return find_peak(list_of_integers[:n]
+        return find_peak(list_of_integers[:n])
     else:
-        return find_peak(list_of_integers[n + 1:]
+        return find_peak(list_of_integers[n + 1:])
 
