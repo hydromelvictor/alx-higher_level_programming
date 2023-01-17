b1ff2f9b149b7fd8835263f12d45b336a1f63889
harkinder-dark <victorvaddely@gmail.com>
2023-01-17 14:29:02 +0000

    python networking

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == "__main__":
-    req = url.request.Request(sys.argv[1])
+    req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
