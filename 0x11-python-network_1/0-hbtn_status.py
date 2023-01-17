7998d99df28626c9f1b87ef6b40153a663efdc12
harkinder-dark <victorvaddely@gmail.com>
2023-01-17 14:18:14 +0000

    python networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == '__main__':
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
-        html = response.read()
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
