ff97bfbefea32d48749ac855b5ed23b15648e2b8
harkinder-dark <victorvaddely@gmail.com>
2023-01-17 13:56:49 +0000

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches 
+"""Write a Python script that fetches
 https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]:
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
